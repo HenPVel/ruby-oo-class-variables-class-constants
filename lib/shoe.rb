2ee65cff9bf9c5ff111b05f08790966d26c20638
Henry <hvelas93@gmail.com>
2021-02-24 23:02:48 -0500

    Finished Class variables class constants

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,27 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    #binding.pry
+    if BRANDS.include?(brand)==false
+    BRANDS << brand
+    end
+    #pp BRANDS
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+  end
+
+  def brand=(brand)
+    @brand = brand
   end
 
 end
